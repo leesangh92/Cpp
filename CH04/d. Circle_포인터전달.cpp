1a1aff4739b74fb36ce32e977fe08fb91afba439
leesangh <83565881+leesangh92@users.noreply.github.com>
2022-03-23 20:16:33 +0900

    Add files via upload

---
+++ CH04/d. Circle_포인터전달.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+class Circle
+{
+private:
+	int radius;
+public:
+	Circle()
+	{
+		radius = 1;
+	}
+
+	Circle(int r)
+	{
+		radius = r;
+	}
+	
+	~Circle()
+	{
+		cout << "~Circle()" << endl;
+	}
+
+	int getRadius()
+	{
+		return radius;
+	}
+
+	double getArea()
+	{
+		return radius * radius * 3.14;
+	}
+};
+
+int main()
+{
+	Circle donut;
+	Circle pizza(30);
+
+	Circle* p = &donut;
+	cout << "p->getArea() : " << p->getArea() << endl;
+
+	p = &pizza;
+	cout << "p->getArea() : " << p->getArea() << endl;
+	
+	// 소멸자는 '객체'(donut과 pizza, p는 임시 변수)를 소멸시킨다. -> 소멸자는 2회 생성
+
+	return 0;
+}
